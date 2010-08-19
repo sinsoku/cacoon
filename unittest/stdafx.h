a24603117b961bb02f8ae09fd9daa9f177f1be57
cohama <hirotaka.hamada.1024@gmail.com>
2010-08-19 22:37:45 +0900

    unittest/stdafx.h と Cacoon/stdafx.h が競合していたのを解消

--- unittest/stdafx.h
+++ unittest/stdafx.h
@@ -1,10 +1,16 @@
 #pragma once
 
 // STL や boost、GoogleTest などのサイズの大きな、かつ、変更されないヘッダファイルはここに追加する。
+// 新しく cpp ファイルを追加する場合は必ずこのヘッダをインクルードする。(テストの有無にかかわらず)
+#include "../Cacoon/stdafx.h"
 
 #include <gtest/gtest.h>
+
 #include <boost/foreach.hpp>
 #include <boost/lexical_cast.hpp>
+#include <boost/utility.hpp>
 
 #pragma comment( lib, "gtestd.lib" )
 #pragma comment( lib, "gtest_maind.lib" )
+
+#pragma comment( lib, "ws2_32.lib" )

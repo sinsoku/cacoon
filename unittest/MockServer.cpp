d800a80ca36f1b54bbaef61daa969e022161d988
sinsoku <sinsoku.listy@gmail.com>
2010-08-24 01:25:57 +0900

    Cacoo APIのページの例と同じデータを作成するメソッドを作成

--- unittest/MockServer.cpp
+++ unittest/MockServer.cpp
@@ -18,22 +18,155 @@ std::string MockServer::get(const std::string& url)
 
 std::string MockServer::createDiagrams()
 {
-	return "";
+	char resXmlData[] =
+		"<?xml version=\"1.0\" encoding=\"utf-8\"?>"
+		""
+		"<diagrams>"
+		"  <result>"
+		"    <diagram>"
+		"      <url>https://cacoo.com/diagrams/00e77f4dc9973517</url>"
+		"      <imageUrl>https://cacoo.com/diagrams/00e77f4dc9973517.png</imageUrl>"
+		""
+		"      <imageUrlForApi>https://cacoo.com/api/v1/diagrams/00e77f4dc9973517.png</imageUrlForApi>"
+		"      <diagramId>00e77f4dc9973517</diagramId>"
+		"      <title>shape1</title>"
+		"      <security>url</security>"
+		""
+		"      <ownerName>yoko</ownerName>"
+		"      <ownerNickname>Yoko</ownerNickname>"
+		"      <owner>"
+		"        <name>yoko</name>"
+		"        <nickname>Yoko</nickname>"
+		"        <imageUrl>https://cacoo.com/account/yoko/image/32x32</imageUrl>"
+		"      </owner>"
+		"      <editing>false</editing>"
+		"      <own>true</own>"
+		""
+		"      <shared>true</shared>"
+		"      <sheetCount>0</sheetCount>"
+		"      <created>Mon, 10 Aug 2009 17:00:00 +0900</created>"
+		"      <updated>Mon, 10 Aug 2009 17:00:00 +0900</updated>"
+		""
+		"    </diagram>"
+		"    <diagram>"
+		"      ..."
+		"    </diagram>"
+		"  </result>"
+		"  <count>2</count>"
+		""
+		"</diagrams>";
+
+	return resXmlData;
 }
 
 std::string MockServer::createDiagram()
 {
-	return "";
+	char resXmlData[] =
+		"<?xml version=\"1.0\" encoding=\"utf-8\"?>"
+		"<diagram>"
+		"  <url>https://cacoo.com/diagrams/00e77f4dc9973517</url>"
+		""
+		"  <imageUrl>https://cacoo.com/diagrams/00e77f4dc9973517.png</imageUrl>"
+		"  <imageUrlForApi>https://cacoo.com/api/v1/diagrams/00e77f4dc9973517.png</imageUrlForApi>"
+		"  <diagramId>00e77f4dc9973517</diagramId>"
+		"  <title>shape1</title>"
+		""
+		"  <security>url</security>"
+		"  <ownerName>yoko</ownerName>"
+		"  <ownerNickname>Yoko</ownerNickname>"
+		"  <owner>"
+		"    <name>yoko</name>"
+		"    <nickname>Yoko</nickname>"
+		"    <imageUrl>https://cacoo.com/account/yoko/image/32x32</imageUrl>"
+		"  </owner>"
+		""
+		"  <own>true</own>"
+		""
+		"  <shared>false</shared>"
+		"  <created>Mon, 10 Aug 2009 17:00:00 +0900</created>"
+		"  <updated>Mon, 10 Aug 2009 17:00:00 +0900</updated>"
+		""
+		"  <sheets>"
+		"    <sheet>"
+		"      <url>https://cacoo.com/diagrams/00e77f4dc9973517#aaaa</url>"
+		"      <imageUrl>https://cacoo.com/diagrams/00e77f4dc9973517-aaaa.png</imageUrl>"
+		"      <imageUrlForApi>https://cacoo.com/api/v1/diagrams/00e77f4dc9973517-aaaa.png</imageUrlForApi>"
+		""
+		"      <uid>aaaa</uid>"
+		"      <name>sheet1</name>"
+		"      <width>200</width>"
+		"      <height>100</height>"
+		""
+		"    </sheet>"
+		"    <sheet>"
+		"      ..."
+		"    </sheet>"
+		"  </sheets>"
+		""
+		"  <comments>"
+		"    <comment>"
+		"      <user>"
+		"        <name>yoko</name>"
+		"        <nickname>Yoko</nickname>"
+		"        <imageUrl>https://cacoo.com/account/yoko/image/32x32</imageUrl>"
+		"      </user>"
+		"      <content>comment 1</content>"
+		"      <created>Mon, 10 Aug 2009 17:00:00 +0900</created>"
+		"      <updated>Mon, 10 Aug 2009 17:00:00 +0900</updated>"
+		"    </comment>"
+		"    <comment>"
+		"      ..."
+		"    </comment>"
+		"  </comments>"
+		"</diagram>";
+
+	return resXmlData;
 }
 
 std::string MockServer::createChatMesseages()
 {
-	return "";
+	char resXmlData[] =
+		"<?xml version=\"1.0\" encoding=\"utf-8\"?>"
+		"<messages>"
+		"  <result>"
+		"    <message>"
+		"      <user>"
+		"        <name>yoko</name>"
+		"        <nickname>Yoko</nickname>"
+		"        <imageUrl>https://cacoo.com/account/yoko/image/32x32</imageUrl>"
+		"      </user>"
+		"      <content>message 1</content>"
+		"      <created>Mon, 10 Aug 2009 17:00:00 +0900</created>"
+		"    </message>"
+		"    <message>"
+		"      ..."
+		"    </message>"
+		"  </result>"
+		"</messages>";
+
+	return resXmlData;
 }
 
 std::string MockServer::createFolders()
 {
-	return "";
+	char resXmlData[] =
+		"<?xml version=\"1.0\" encoding=\"utf-8\"?>"
+		"<folders>"
+		"  <result>"
+		"    <folder>"
+		"      <folderId>10001lt;/folderId>"
+		"      <folderName>Folder 1</folderName>"
+		"      <type>normal</type>"
+		"      <created>Mon, 10 Aug 2009 17:00:00 +0900</created>"
+		"      <updated>Mon, 10 Aug 2009 17:00:00 +0900</updated>"
+		"    </folder>"
+		"    <folder>"
+		"      ..."
+		"    </folder>"
+		"  </result>"
+		"</folders>";
+
+	return resXmlData;
 }
 
 std::string MockServer::createUsers()
@@ -41,9 +174,9 @@ std::string MockServer::createUsers()
 	char resXmlData[] =
 		"<?xml version=\"1.0\" encoding=\"utf-8\"?>"
 		"<user>"
-		"<name>yoko</name>"
-		"<nickname>Yoko</nickname>"
-		"<imageUrl>https://cacoo.com/account/yoko/image/32x32</imageUrl>"
+		"  <name>yoko</name>"
+		"  <nickname>Yoko</nickname>"
+		"  <imageUrl>https://cacoo.com/account/yoko/image/32x32</imageUrl>"
 		"</user>";
 
 	return resXmlData;
@@ -51,5 +184,13 @@ std::string MockServer::createUsers()
 
 std::string MockServer::createAccount()
 {
-	return "";
+	char resXmlData[] =
+		"<?xml version=\"1.0\" encoding=\"utf-8\"?>"
+		"<account>"
+		"  <name>yoko</name>"
+		"  <nickname>Yoko</nickname>"
+		"  <imageUrl>https://cacoo.com/account/yoshizawa/image/32x32</imageUrl>"
+		"</account>";
+
+	return resXmlData;
 }

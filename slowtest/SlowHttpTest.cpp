04b4c0c24af6775de6aba2f35cb3f41ee63ca9bf
cohama <hirotaka.hamada.1024@gmail.com>
2010-08-27 00:28:23 +0900

    実際に HTTP 通信を行ってテストする SlowHttpTest.cpp を追加。

--- slowtest/SlowHttpTest.cpp
+++ slowtest/SlowHttpTest.cpp
@@ -4,20 +4,22 @@
 #include "HeaderMap.h"
 
 // 複数回のレスポンスのテスト
-TEST( HttpConnectionTest, MultiRequest )
-{
-	EXPECT_NO_THROW(
-	{
-		Connection c = HttpClient::CreateHttpConnection( "cacoo.com" );
-		Response res1st = c.Request( Method::Get, "/" );
-		Response res2nd = c.Request( Method::Get, "/features" );
-	} );
-}
+//TEST( HttpConnectionTest, MultiRequest )
+//{
+//	EXPECT_NO_THROW(
+//	{
+//		Connection c = HttpClient::CreateHttpConnection( "cacoo.com" );
+//		Response res1st = c.Request( Method::Get, "/" );
+//		Response res2nd = c.Request( Method::Get, "/features" );
+//	} );
+//}
 
 // 実際に HTTP 通信をしてみる
 TEST( HttpConnectionTest, RealConnection )
 {
 	Connection c = HttpClient::CreateHttpConnection( "www.google.co.jp" );
 	Response res = c.Request( Method::Get, "/index.html" );
+	std::cout << res.Header().ToString() << "\r\n" << res.Body() << std::endl;
 	ASSERT_EQ( res.StatusCode(), 200 );
+
 }

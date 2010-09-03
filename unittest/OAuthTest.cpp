2bd2d36bf7f7e1417e1f64df26ea0f6090c9249c
cohama <hirotaka.hamada.1024@gmail.com>
2010-09-04 03:25:36 +0900

    OAuth の認証に必要な HMAC-SHA1 による暗号化と Base64 エンコーディングについてのテストを追加。

---
+++ unittest/OAuthTest.cpp
@@ -0,0 +1,62 @@
+#include "stdafx.h"
+#include "HttpClient.h"
+
+// OpenSSL HMAC-SHA1 + Base64 エンコーディング
+TEST( OAuthTest, HmacSha1Base64 )
+{
+	size_t resLength;
+	unsigned char res[SHA_DIGEST_LENGTH + 1];
+
+	char key[] = "1";
+	int keyLength = strlen( key );
+
+	unsigned char data[] = "012345";
+	size_t dataLength = sizeof( data ) - 1;
+
+	HMAC( EVP_sha1(), key, keyLength, data, dataLength, res, &resLength );
+
+	char b64[256];
+	DWORD b64Length;
+
+	CryptBinaryToStringA( res, resLength, CRYPT_STRING_BASE64, b64, &b64Length );
+
+	// 変換した文字列の最後に CRLF がついてくるのでそれをとる。
+	char b64withoutCRLF[256];
+	int b64length = strlen( b64 );
+	for( int i=0; i<b64length-2; i++ )	// CRLF 分が -2
+	{
+		b64withoutCRLF[i] = b64[i];
+	}
+	b64withoutCRLF[b64length-2] = 0;	// null 文字
+	std::cout << b64withoutCRLF << std::endl;
+	
+	// Python で作った値と比較。
+	EXPECT_STREQ( "uSAq0a63u1W41eTdCrgbywm5CU8=", b64withoutCRLF );
+}
+
+// OpenSSL HMAC-SHA1 のテスト
+TEST( OAuthTest, HmacSha1 )
+{
+	size_t resLength;
+	unsigned char res[SHA_DIGEST_LENGTH + 1];
+
+	char key[] = "1";
+	int keyLength = strlen( key );
+
+	unsigned char data[] = "012345";
+	size_t dataLength = sizeof( data ) - 1;
+
+	HMAC( EVP_sha1(), key, keyLength, data, dataLength, res, &resLength );
+
+	std::ostringstream oss;
+
+	for( int i=0; i<(int)sizeof( res ) - 1; i++ )
+	{
+		oss << boost::format( "%02x" ) % (int)res[i];
+	}
+
+	std::cout << oss.str() << std::endl;
+	
+	// Python で作った場合と比較
+	EXPECT_STREQ( "b9202ad1aeb7bb55b8d5e4dd0ab81bcb09b9094f", oss.str().c_str() );
+}

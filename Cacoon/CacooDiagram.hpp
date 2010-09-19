e40687e985d81eff3ad577f1838d26fcdba0bf1e
sinsoku <sinsoku.listy@gmail.com>
2010-09-20 02:08:49 +0900

    Diagramは必要最小限のゲッターのみ実装

--- Cacoon/CacooDiagram.hpp
+++ Cacoon/CacooDiagram.hpp
@@ -37,6 +37,8 @@ public:
 		while( e != NULL ) {
 			if ( !strcmp( e->Value(), "owner") ) {
 				data[e->Value()] = CacooUser(e);
+			} else if ( !strcmp( e->Value(), "created") || !strcmp( e->Value(), "updated") ) {
+				data[e->Value()] = timeFromString( e->GetText() );
 			} else if ( !strcmp( e->Value(), "sheets") ) {
 				// –¢ŽÀ‘•‚Ì‚½‚ßA”ò‚Î‚·
 			} else if ( !strcmp( e->Value(), "comments") ) {
@@ -54,12 +56,37 @@ public:
 	~CacooDiagram() {}
 
 	time_t getCreated() {
-		std::string created = boost::any_cast<std::string>(data["created"]);
-		return 0; // fake it
+		return boost::any_cast<time_t>(data["created"]);
 	}
 
 	time_t getUpdated() {
-		std::string updated = boost::any_cast<std::string>(data["updated"]);
-		return 0; // fake it
+		return boost::any_cast<time_t>(data["updated"]);
+	}
+
+	static time_t timeFromString(const std::string& strdate) {
+		const std::map<std::string, int> MONTH_MAP = boost::assign::map_list_of
+			("Jan", 1)("Feb", 2)("Mar", 3)("Apr", 4)("May", 5)("Jun", 6)
+			("Jul", 7)("Aug", 8)("Sep", 9)("Oct", 10)("Nov", 11)("Dec", 12);
+		std::vector<std::string> date;
+		parseDate(date, strdate);
+
+		struct tm tm_date;
+		tm_date.tm_year = boost::lexical_cast<int>( date[3] ) - 1900;
+		tm_date.tm_mon  = MONTH_MAP.find(date[2])->second - 1;
+		tm_date.tm_mday = boost::lexical_cast<int>( date[1] );
+		tm_date.tm_hour = boost::lexical_cast<int>( date[4] );
+		tm_date.tm_min  = boost::lexical_cast<int>( date[5] );
+		tm_date.tm_sec  = boost::lexical_cast<int>( date[6] );
+
+		return mktime(&tm_date);
+	}
+
+	static void parseDate(std::vector<std::string>& sv, const std::string& strdate) {
+		char_separator sep(" ,:", "", boost::drop_empty_tokens);
+		tokenizer tokens(strdate, sep);
+
+		BOOST_FOREACH(std::string v, tokens) {
+			sv.push_back( v );
+		}
 	}
 };
--- unittest/CacooDiagramTest.cpp
+++ unittest/CacooDiagramTest.cpp
@@ -1,7 +1,50 @@
 #include "stdafx.h"
 
+#include "CacooApi.h"
 #include "CacooDiagram.hpp"
 
 TEST(CacooDiagram, init)
 {
+	char rawXmlData[] =
+		"<diagram>"
+		"  <url>https://cacoo.com/diagrams/00e77f4dc9973517</url>"
+		"  <imageUrl>https://cacoo.com/diagrams/00e77f4dc9973517.png</imageUrl>"
+		"  <imageUrlForApi>https://cacoo.com/api/v1/diagrams/00e77f4dc9973517.png</imageUrlForApi>"
+		"  <diagramId>00e77f4dc9973517</diagramId>"
+		"  <title>shape1</title>"
+		"  <security>url</security>"
+		"  <ownerName>yoko</ownerName>"
+		"  <ownerNickname>Yoko</ownerNickname>"
+		"  <owner>"
+		"    <name>yoko</name>"
+		"    <nickname>Yoko</nickname>"
+		"    <imageUrl>https://cacoo.com/account/yoko/image/32x32</imageUrl>"
+		"  </owner>"
+		"  <editing>false</editing>"
+		"  <own>true</own>"
+		"  <shared>true</shared>"
+		"  <sheetCount>0</sheetCount>"
+		"  <created>Mon, 10 Aug 2009 17:00:00 +0900</created>"
+		"  <updated>Mon, 10 Aug 2009 17:00:00 +0900</updated>"
+		"</diagram>";
+	boost::shared_ptr<TiXmlDocument> doc = CacooApi::parseXml(rawXmlData);
+	CacooDiagram actual( doc->RootElement() );
+
+	time_t t = actual.getCreated();
+	struct tm* cal = localtime( &t );
+	EXPECT_EQ(2009, cal->tm_year + 1900);
+	EXPECT_EQ(8,    cal->tm_mon + 1);
+	EXPECT_EQ(10,   cal->tm_mday);
+	EXPECT_EQ(17,   cal->tm_hour);
+	EXPECT_EQ(0, cal->tm_min);
+	EXPECT_EQ(0, cal->tm_sec);
+
+	time_t t2 = actual.getCreated();
+	struct tm* cal2 = localtime( &t2 );
+	EXPECT_EQ(2009, cal2->tm_year + 1900);
+	EXPECT_EQ(8,    cal2->tm_mon + 1);
+	EXPECT_EQ(10,   cal2->tm_mday);
+	EXPECT_EQ(17,   cal2->tm_hour);
+	EXPECT_EQ(0,    cal2->tm_min);
+	EXPECT_EQ(0,    cal2->tm_sec);
 }
